5683db9bf6e0b45908794a5d6be45bc68dd5fdc4
Mohamed youssef <mohamed.youssef@fawry.com>
2022-03-03 13:44:47 +0200

    Creating custom TableViewCell from the upcoming table

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Joe on 03/03/2022.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}

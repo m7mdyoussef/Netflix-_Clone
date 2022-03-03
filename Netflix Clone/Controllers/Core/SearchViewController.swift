06b15da9e74f2396f760c1d97734fa22f29c9f0a
Mohamed youssef <mohamed.youssef@fawry.com>
2022-03-03 14:17:33 +0200

    Creating SearchResultsViewController to display search results

--- Netflix Clone/Controllers/Core/SearchViewController.swift
+++ Netflix Clone/Controllers/Core/SearchViewController.swift
@@ -17,6 +17,12 @@ class SearchViewController: UIViewController {
         return table
     }()
 
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +34,8 @@ class SearchViewController: UIViewController {
         view.addSubview(discoverTable)
         discoverTable.delegate = self
         discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
         
         fetchDiscoverdMovies()
     }
---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Joe on 03/03/2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+
+}
